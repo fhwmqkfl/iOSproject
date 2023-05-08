6961bcacf2529efc7ec00edd889329d833dd9dcd
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-05-08 18:49:08 +0900

    feat(PageControl) : page move with segue

--- PageControl/PageControl/ThirdViewController.swift
+++ PageControl/PageControl/ThirdViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    var someString: String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainLabel.text = someString
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonTappted(_ sender: UIButton) {
+        dismiss(animated: true)
     }
-    */
 
 }
--- PageControl/PageControl/ViewController.swift
+++ PageControl/PageControl/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    //  1. 코드로 화면이동
+    // 1. 코드로 화면이동
     // 다음화면(nextVC)객체 생성 -> present
     @IBAction func codeNextButtonTapped(_ sender: UIButton) {
         let FirstVC = FirstViewController()
@@ -34,7 +34,8 @@ class ViewController: UIViewController {
         present(FirstVC, animated: true)
     }
     
-    //  2. 코드로 스토리보드 객체를 생성해, 화면이동
+    // 2. 코드로 스토리보드 객체를 생성해, 화면이동
+    // 다음화면(nextVC)객체 생성 -> present
     @IBAction func storyboardWithCodeButtonTapped(_ sender: UIButton) {
         
         let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
@@ -43,8 +44,36 @@ class ViewController: UIViewController {
         self.present(nextVC, animated: true)
     }
     
-    //  3. 스토리보드에서의 화면이동(간접세그웨이)
+    // 3. 스토리보드에서의 화면이동(간접세그웨이)
+    // segue만들기 -> (performSegue) -> prepare
     @IBAction func storyboardWithSegueButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "toThirdVC", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toThirdVC" {
+            let thirdVC = segue.destination as! ThirdViewController
+            // 데이터 전달
+            thirdVC.someString = "엄마상어"
+        }
+        
+        if segue.identifier == "toFourthVC" {
+            let fourthVC = segue.destination as! FourthViewController
+            fourthVC.someString = "뚜루루뚜뚜"
+        }
+    }
+    
+    
+    var num = 3
+    // 4) 버튼에서 직접적으로 연결되었을때 세그웨이
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if num > 2 {
+            // 다음화면으로 넘어가지 않음
+            return false
+        } else {
+            return true
+        }
     }
     
     
