852951e45c5eabffc6e7b276afe3cde9c29e926f
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-05-08 17:55:48 +0900

    add(PageControl) : uiButton setup

---
+++ PageControl/PageControl/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by coco on 2023/05/08.
+//
+/*
+ 연습 4가지
+ 1. 코드로 화면이동
+ 2. 코드로 스토리보드 객체를 생성해, 화면이동
+ 3. 스토리보드에서의 화면이동(간접세그웨이)
+ 4. 스토리보드에서의 화면이동(직접세그웨이)
+
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //  1. 코드로 화면이동
+    @IBAction func codeNextButtonTapped(_ sender: UIButton) {
+    }
+    
+    //  2. 코드로 스토리보드 객체를 생성해, 화면이동
+    @IBAction func storyboardWithCodeButtonTapped(_ sender: UIButton) {
+    }
+    
+    //  3. 스토리보드에서의 화면이동(간접세그웨이)
+    @IBAction func storyboardWithSegueButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+}
+
