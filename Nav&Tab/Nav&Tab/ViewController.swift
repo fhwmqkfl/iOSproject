feab16774c66a1c8f5c75fc01a06f4f00c928a32
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-05-10 00:30:22 +0900

    add(Nav&Tab) : practice with storyboard

---
+++ Nav&Tab/Nav&Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by coco on 2023/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
