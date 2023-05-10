73a69748e111c4f8d5df48834ed5e73c0c80c29b
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-05-10 22:12:51 +0900

    add(TableView): init project

---
+++ TableView/TableView/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  TableView
+//
+//  Created by coco on 2023/05/10.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
