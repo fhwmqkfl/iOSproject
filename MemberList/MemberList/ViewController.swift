ef021fed4a33735fb4f2207910aca4d8ac83da52
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-05-11 00:56:09 +0900

    add(MemberList) : init project

---
+++ MemberList/MemberList/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  MemberList
+//
+//  Created by coco on 2023/05/11.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    private let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        setConstraints()
+    }
+    
+    func setConstraints() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
