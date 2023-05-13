1dde17774f327c15701c2f8dc0f78ffa9793edf7
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-05-14 00:55:31 +0900

    add(ToDoApp) : init project with coredata

---
+++ ToDoApp/ToDoApp/ToDoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoData+CoreDataClass.swift
+//  ToDoApp
+//
+//  Created by coco on 2023/05/14.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoData)
+public class ToDoData: NSManagedObject {
+
+}
